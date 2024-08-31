00e0165ef54a7c980af228e1e6d55aa0de68a186
markeloss <eriquepinon@gmail.com>
2024-08-31 18:43:10 -0300

    agora vai

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,19 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+const withImages = require('next-images');
+
+module.exports = withImages({
+  images: {
+    domains: ['your-image-host.com'],
+  },
+  env: {
+    GITHUB_API_URL: process.env.GITHUB_API_URL,
+  },
+  webpack: (config) => {
+    // Configurações personalizadas para o webpack
+    return config;
+  },
+});
